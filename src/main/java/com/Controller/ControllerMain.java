6b5ce7bacadf7da179a051e216b7d9558da236a8
Pier-Hugo <pierhugocarpentier36@gmail.com>
2020-01-22 16:40:01 +0100

    Ajout de JavaFX

--- src/main/java/com/Controller/ControllerMain.java
+++ src/main/java/com/Controller/ControllerMain.java
@@ -1,6 +1,18 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class ControllerMain
 {
+    @FXML
+    private Button HelloWorldButton;
+
+    @FXML
+    void printHelloWorld(ActionEvent event)
+    {
+        System.out.println("HelloWorld!");
+    }
 
 }
---
+++ test/test/ManagerTestTest.java
@@ -0,0 +1,73 @@
+package test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * ManagerTest Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>janv. 16, 2020</pre>
+ */
+public class ManagerTestTest
+{
+
+    @Before
+    public void before() throws Exception
+    {
+    }
+
+    @After
+    public void after() throws Exception
+    {
+    }
+
+    /**
+     * Method: addSeller()
+     */
+    @Test
+    public void testAddSeller() throws Exception
+    {
+//TODO: Test goes here... 
+    }
+
+    /**
+     * Method: removeSeller()
+     */
+    @Test
+    public void testRemoveSeller() throws Exception
+    {
+//TODO: Test goes here... 
+    }
+
+    /**
+     * Method: editDepartment()
+     */
+    @Test
+    public void testEditDepartment() throws Exception
+    {
+//TODO: Test goes here... 
+    }
+
+    /**
+     * Method: editMail()
+     */
+    @Test
+    public void testEditMail() throws Exception
+    {
+//TODO: Test goes here... 
+    }
+
+    /**
+     * Method: editShopName()
+     */
+    @Test
+    public void testEditShopName() throws Exception
+    {
+//TODO: Test goes here... 
+    }
+
+
+} 
