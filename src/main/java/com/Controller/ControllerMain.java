984248c5c6c535b81b2768300390319c1af9b0fd
ErwannPp <epiedloup92@gmail.com>
2020-01-23 11:00:35 +0100

    assemblage vue + bdd

--- src/main/java/com/Controller/ControllerMain.java
+++ src/main/java/com/Controller/ControllerMain.java
@@ -2,17 +2,8 @@ package Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 
 public class ControllerMain
 {
-    @FXML
-    private Button HelloWorldButton;
-
-    @FXML
-    void printHelloWorld(ActionEvent event)
-    {
-        System.out.println("HelloWorld!");
-    }
 
 }
--- src/main/java/com/Main/Main.java
+++ src/main/java/com/Main/Main.java
@@ -5,14 +5,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import java.io.IOException;
+
 public class Main extends Application
 {
+    private Stage primaryStage;
+    private BorderPane viewMain;
+    
 
     public static void main(String[] args)
     {
@@ -26,24 +33,51 @@ public class Main extends Application
         {
             System.out.println("Erreur lors de la création de la base de données ou des données :\n-> " + e);
         }
-
-        /* Application */
-        try
-        {
-            launch(args);
-        } catch (Exception e)
-        {
-            System.out.println("Erreur lors de l'utilisation de l'application :\n-> " + e);
-        }
-
+        launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception
+    public void start(Stage primaryStage) 
     {
-        Parent root = FXMLLoader.load(getClass().getResource("/View/ViewMain.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Stock Manager");
+
+        initViewMain();
+
+        showConnectionView();
+    }
+
+
+
+    public void initViewMain() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("/View/ViewMain.fxml"));
+            viewMain = (BorderPane) loader.load();
+
+            Scene scene = new Scene(viewMain);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showConnectionView() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("/View/ConnectionView.fxml"));
+            AnchorPane connectionView = (AnchorPane) loader.load();
+
+            viewMain.setCenter(connectionView);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
     }
 }
